6e3e8fc5a96454b283c91946f7480c8af1a440d2
qbentil <bentilshadrack72@gmail.com>
2022-11-15 10:56:24 +0000

    Update dumy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import  { useState } from "react";
+import { useState } from "react";
 import ImageBox from "./components/image-box";
 import Uploader from "./components/uploader";
 
-const App = () => {
+const ReactImageUploader = () => {
   const [image, setImage] = useState(null);
-  const [imageURL, setImageURL] = useState('');
+  const [imageURL, setImageURL] = useState("");
   return (
-    <main className='w-screen h-screen bg-orange-400 flex items-center justify-center'>
-          <div className={'w-72 h-72'}>
-      {image ? (
-        <ImageBox setImage={setImage} imageURL={imageURL} />
-      ) : (
-        <Uploader setImageURL={setImageURL} setImage={setImage} />
-      )}
-    </div>
+    <main className="w-screen h-screen bg-orange-400 flex items-center justify-center">
+      <div className="w-96 h-72">
+        {image ? (
+          <ImageBox
+            setImage={setImage}
+            setImageURL={setImageURL}
+            imageURL={imageURL}
+          />
+        ) : (
+          <Uploader setImageURL={setImageURL} setImage={setImage} />
+        )}
+      </div>
     </main>
   );
-}
+};
 
-export default App;
+export default ReactImageUploader;
